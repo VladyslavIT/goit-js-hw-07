8637bc593fcdd448928415415ac44b972207a8fc
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-07-27 15:21:46 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryHTML = galleryItems.map(galleryItemsMurkup).join("");
+galleryEl.insertAdjacentHTML("beforeend", galleryHTML);
+
+function galleryItemsMurkup({ preview, original, description }) {
+  return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+}
+
+const onGallaryClick = (event) => {
+  event.preventDefault();
+  const srcImg = event.target.dataset.source;
+ console.log(srcImg);
+};
+
+galleryEl.addEventListener("click", onGallaryClick);
+
+const onOpenImage = (srcImg) => {
+  const instance = basicLightbox.create(`
+     <img
+            class="gallery__image"
+            src="${srcImg}"
+            alt="original"
+          />
+`);
+  instance.show()
+  console.log(instance.element().querySelector('img').src);
+};
+
+galleryEl.addEventListener("click", onOpenImage);
